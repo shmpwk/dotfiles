e26233c113bd0bbc2cb3eef1657c6c19d78b7720
Shumpei Wakabayashi <shumpei.wakabayashi@tier4.jp>
2022-04-23 22:45:14 +0900

    feat(vscode): add extention install

--- vscode/extensions/ms-iot.vscode-ros-0.8.2/out/src/debugger/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Copyright (c) Andrew Short. All rights reserved.
-// Licensed under the MIT License.
-Object.defineProperty(exports, "__esModule", { value: true });
-const session = require("./debug-session");
-session.RosDebugSession.run(session.RosDebugSession);
-//# sourceMappingURL=main.js.map
